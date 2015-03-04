897090abf886710062de720601ad9424df222bce
kodmunki <kodmunki@gmail.com>
2015-03-04 15:16:20 -0600

    Updates
    Complete redo of the process due to Apple no longer validating fat
    binaries

---
+++ lib/Debug/ku4objc_tempDependent.framework/Headers/subClassTemp.h
@@ -0,0 +1,14 @@
+//
+//  subClassTemp.h
+//  ku4objc_tempDependent
+//
+//  Created by kodmunki on 02/01/2015.
+//  Copyright (c) 2015 kodmunki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <ku4objc_temp/ku4objc_temp.h>
+
+@interface subClassTemp : classTemp
+
+@end
